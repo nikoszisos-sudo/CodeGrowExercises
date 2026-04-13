8592d4343337239165f581ea9ffb6abe731588de
Nikolaos Zisos <nikoszisos@gmail.com>
2026-04-13 18:00:48 +0300

    Initial commit

---
+++ break_continue_pass.py
@@ -0,0 +1,34 @@
+#break stops the loop
+#while true ekteleitai synexeia
+
+#while True:
+#    symbol = input("Enter a name: ")
+#    if symbol != "":
+#        print(symbol)
+#        break
+
+#---------------------------------------------------------
+
+#continue skip the current iteration (epalanipsi)
+
+#symbol = "Niko-l-a-os"
+#for symb in symbol:
+#    if symb == "-":
+#        continue
+#    print(symb, end="")
+
+#--------------------------------------------------------
+
+#pass does nothing
+
+#for i in range(1,21):
+#    if i == 13:
+#        pass
+#    else:
+#        print(i, end=" ")
+
+
+
+
+
+
